d9caea203ac56f672a44254c85f4076ac12de454
Chang Wang <garnwraly@gmail.com>
2017-11-14 12:33:13 -0500

    read from .env

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
 
 import { start } from './server';
+
+dotenv.configuire();
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
