a01093f34b466bf2f7ea6256c2656fa945e98a5d
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-11-15 10:37:09 -0500

    update queries

--- graphql/index.ts
+++ graphql/index.ts
@@ -2,27 +2,17 @@ import { composeWithMongoose } from 'graphql-compose-mongoose';
 import { GQC } from 'graphql-compose';
 import { UserModel } from '../models/UserProfile';
 
-const customizationOptions = {};
-const UserTC = composeWithMongoose(UserModel, customizationOptions);
+const UserTC = composeWithMongoose(UserModel, {});
 
 GQC.rootQuery().addFields({
-  userById: UserTC.getResolver('findById'),
-  userMany: UserTC.getResolver('findMany'),
-  userByIds: UserTC.getResolver('findByIds'),
-  userOne: UserTC.getResolver('findOne'),
-  userCount: UserTC.getResolver('count'),
-  userConnection: UserTC.getResolver('connection'),
-  userPagination: UserTC.getResolver('pagination'),
+  user: UserTC.getResolver('findById'),
+  users: UserTC.getResolver('pagination'),
 });
 
 GQC.rootMutation().addFields({
   userCreate: UserTC.getResolver('createOne'),
-  userUpdateById: UserTC.getResolver('updateById'),
-  userUpdateOne: UserTC.getResolver('updateOne'),
-  userUpdateMany: UserTC.getResolver('updateMany'),
-  userRemoveById: UserTC.getResolver('removeById'),
-  userRemoveOne: UserTC.getResolver('removeOne'),
-  userRemoveMany: UserTC.getResolver('removeMany'),
+  userUpdate: UserTC.getResolver('updateById'),
+  userRemove: UserTC.getResolver('removeById'),
 });
 
 export const schema = GQC.buildSchema();
