1b4bc14c40921da6ba833a72438ebd8d39098f86
Jonathan Ganesh <jganesh@olm-jganesh.local>
2018-03-29 15:17:19 -0400

    Correct TF parameters

--- personaservice-api/deploy_stage/deploy.sh
+++ personaservice-api/deploy_stage/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $1 = "dev" ]; then
-  rm -rf persona*
-  git clone git@github.com:overture-stack/persona.git
-  cd persona/
+  rm -rf aws-ecs-service-*
+  git clone git@github.com:kids-first/aws-ecs-service-type-1.git
+  cd aws-ecs-service-type-1/
   echo "Setting up backend"
-  echo 'key        = "dev/kf-dev-pi-personaserverservice-us-east-1-RSF"' >> dev.conf
+  echo 'key        = "dev/kf-dev-api-personaserverservice-us-east-1-RSF"' >> dev.conf
   terraform init -backend=true -backend-config=dev.conf
    validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaserverservice:latest' \
    -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-dev"' -var 'application=personaserverservice-api' \
@@ -18,9 +18,9 @@ if [ $1 = "dev" ]; then
 fi
 
 if [ $1 = "qa" ]; then
-  rm -rf persona*
-  git clone git@github.com:overture-stack/persona.git
-  cd persona/
+  rm -rf aws-ecs-service-*
+  git clone git@github.com:kids-first/aws-ecs-service-type-1.git
+  cd aws-ecs-service-type-1/
   echo "Setting up backend"
   echo 'key        = "qa/kf-qa-api-personaserverservice-us-east-1-RSF"' >> qa.conf
   terraform init -backend=true -backend-config=qa.conf
