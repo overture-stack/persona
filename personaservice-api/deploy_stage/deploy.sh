6af352e31ebe4b9c2530a92bc1648dd5b3de3544
Jonathan Ganesh <jganesh@olm-jganesh.ad.oicr.on.ca>
2018-04-10 14:04:54 -0400

    shorten app name to accommodate load balancer

--- personaservice-api/deploy_stage/deploy.sh
+++ personaservice-api/deploy_stage/deploy.sh
@@ -5,16 +5,16 @@ if [ $1 = "dev" ]; then
   git clone git@github.com:kids-first/aws-ecs-service-type-1.git
   cd aws-ecs-service-type-1/
   echo "Setting up backend"
-  echo 'key        = "dev/kf-dev-api-personaserverservice-us-east-1-RSF"' >> dev.conf
+  echo 'key        = "dev/kf-dev-api-personaservice-us-east-1-RSF"' >> dev.conf
   terraform init -backend=true -backend-config=dev.conf
-  terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-dev"' -var 'application=personaserverservice-api' \
-   -var 'service_name="kf-api-personaserverservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
-   -var 'vault_role="kf_personaserverservice_api_role"'
-  terraform apply --auto-approve -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-dev"' -var 'application=personaserverservice-api' \
-   -var 'service_name="kf-api-personaserverservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
-   -var 'vault_role="kf_personaserverservice_api_role"'
+  terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaservice:latest' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-dev"' -var 'application=personaservice-api' \
+   -var 'service_name="kf-api-personaservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
+   -var 'vault_role="kf_personaservice_api_role"'
+  terraform apply --auto-approve -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaservice:latest' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-dev"' -var 'application=personaservice-api' \
+   -var 'service_name="kf-api-personaservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
+   -var 'vault_role="kf_personaservice_api_role"'
 fi
 
 if [ $1 = "qa" ]; then
@@ -22,14 +22,14 @@ if [ $1 = "qa" ]; then
   git clone git@github.com:kids-first/aws-ecs-service-type-1.git
   cd aws-ecs-service-type-1/
   echo "Setting up backend"
-  echo 'key        = "qa/kf-qa-api-personaserverservice-us-east-1-RSF"' >> qa.conf
+  echo 'key        = "qa/kf-qa-api-personaservice-us-east-1-RSF"' >> qa.conf
   terraform init -backend=true -backend-config=qa.conf
-  terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-qa"' -var 'application=personaserverservice-api' \
-   -var 'service_name="kf-api-personaserverservice"' -var 'owner="jenkins"' -var-file=qa.tfvar \
-   -var 'vault_role="kf_personaserverservice_api_role"'
-  terraform apply --auto-approve -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaserverservice:latest' \
-   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-qa"' -var 'application=personaserverservice-api' \
-   -var 'service_name="kf-api-personaserverservice"' -var 'owner="jenkins"' -var-file=qa.tfvar \
-   -var 'vault_role="kf_personaserverservice_api_role"'
+  terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaservice:latest' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-qa"' -var 'application=personaservice-api' \
+   -var 'service_name="kf-api-personaservice"' -var 'owner="jenkins"' -var-file=qa.tfvar \
+   -var 'vault_role="kf_personaservice_api_role"'
+  terraform apply --auto-approve -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-personaservice:latest' \
+   -var 'task_role_arn="arn:aws:iam::538745987955:role/kfPersonaserviceApiRole-qa"' -var 'application=personaservice-api' \
+   -var 'service_name="kf-api-personaservice"' -var 'owner="jenkins"' -var-file=qa.tfvar \
+   -var 'vault_role="kf_personaservice_api_role"'
 fi
