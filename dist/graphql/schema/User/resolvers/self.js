07c831ef9d1f618bafefb685c8a636e7cfcb6b54
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-11 16:29:56 -0400

    :fire: didn't mean to check-in dist/

--- dist/graphql/schema/User/resolvers/self.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var self = function (UserModel) { return function (_a) {
-    var context = _a.context;
-    return UserModel.findOne({ egoId: context.jwt.sub });
-}; };
-exports.default = self;
-//# sourceMappingURL=self.js.map
--- dist/services/mongoose.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-exports.default = mongoose;
-//# sourceMappingURL=mongoose.js.map
