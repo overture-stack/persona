dc87aa4afb4fc707e74759efb05ef98a567d69cb
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-11 16:29:56 -0400

    :fire: didn't mean to check-in dist/

--- dist/graphql/schema/User/resolvers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var self_1 = require("./self");
-exports.self = self_1.default;
-//# sourceMappingURL=index.js.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-exports.default = server_1.default;
-//# sourceMappingURL=index.js.map
