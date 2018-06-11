80f023a7805bcc473f22f53b1fc4acc05c205507
Minh <hlminh2000@gmail.com>
2018-06-11 11:11:42 -0400

    vault integration for mailchimp stuff

--- services/subscriptionHandler/mailChimpApi.ts
+++ services/subscriptionHandler/mailChimpApi.ts
@@ -2,6 +2,7 @@ import * as md5 from 'crypto-js/md5';
 import * as fetch from 'node-fetch';
 import vault from '../vault';
 
+// TODO: hook into global config, this is kept self-contained for easier refactoring if needed
 let config = {
   vaultSecretPath: process.env.VAULT_APP_SECRET_PATH || 'secret/app',
   kfMailchimpListId: process.env.KF_MAILCHIMP_LIST_ID || '',
@@ -11,12 +12,11 @@ let config = {
 
 export const newMailchimpSubscription = async ({ user }) => {
   const { kfMailchimpListId, kfMailchimpApiKey, kfMailchimpUserName } = config;
-
   const hash = md5(user.email.toLowerCase()).toString();
   const mailChimpDataCenter = kfMailchimpApiKey.split('-')[1];
   const buff = new Buffer(`${kfMailchimpUserName}:${kfMailchimpApiKey}`);
   const b64 = buff.toString('base64');
-  const response = await fetch(
+  return await fetch(
     `https://${mailChimpDataCenter}.api.mailchimp.com/3.0/lists/${kfMailchimpListId}/members/`,
     {
       method: 'POST',
@@ -33,10 +33,15 @@ export const newMailchimpSubscription = async ({ user }) => {
       }),
     },
   ).then(res => res.json());
-  console.log('response: ', response);
 };
 
 export const retrieveMailchimpSecret = () =>
-  vault.getSecretValue(config.vaultSecretPath).then(data => {
-    config = { ...config, ...data };
-  });
+  vault
+    .getSecretValue(config.vaultSecretPath)
+    .then(({ kfMailchimpApiKey, kfMailchimpUserName }) => {
+      config = {
+        ...config,
+        kfMailchimpApiKey,
+        kfMailchimpUserName,
+      };
+    });
