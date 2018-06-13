63377fc22f575fc5b61e5d22025467196e1459e9
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-13 11:39:40 -0400

    :sparkles: user / users resolvers will only return public profiles

--- src/graphql/schema/User/index.ts
+++ src/graphql/schema/User/index.ts
@@ -1,16 +1,34 @@
 import { composeWithMongoose } from 'graphql-compose-mongoose';
+import { set } from 'lodash';
 
 import { self as selfResolver } from './resolvers';
 
-export default ({ models }) => {
-  const UserTC = composeWithMongoose(models.User, {});
+export default ({ models: { User } }) => {
+  const UserTC = composeWithMongoose(User, {});
 
   UserTC.addResolver({
     kind: 'query',
     name: 'self',
     type: UserTC,
-    resolve: selfResolver(models.User),
+    resolve: selfResolver(User),
   });
+
+  UserTC.setResolver(
+    'protectedFindById',
+    UserTC.getResolver('findById').wrapResolve(next => async rp => {
+      const result = await next(rp);
+      return result && (await User.findById(rp.args._id)).isPublic
+        ? result
+        : null;
+    }),
+  );
+
+  UserTC.setResolver(
+    'protectedPagination',
+    UserTC.getResolver('pagination').wrapResolve(next => rp =>
+      next(set(rp, 'args.filter', { ...rp.args.filter, isPublic: true })),
+    ),
+  );
 
   return UserTC;
 };
