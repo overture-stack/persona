68ffb0a2992054b37600d0df0b6740549f6af474
Jon Eubank <joneubank@gmail.com>
2019-07-17 17:08:18 -0400

    :pushpin: Updating token middleware to @overture-stack

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
-import egoTokenMiddleware from 'ego-token-middleware';
+import egoTokenMiddleware from '@overture-stack/ego-token-middleware';
 import { get } from 'lodash';
 
 import createSchema from './graphql';
